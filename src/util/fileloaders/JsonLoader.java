57ea084a7c2905e46f9f1587370571c05edad5ff
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-16 14:31:13 +0000

    Implemented a basic JsonLoader class for loading config files with a simple test. Implemented very basic fields in necessary classes for testing.

---
+++ src/util/fileloaders/JsonLoader.java
@@ -0,0 +1,28 @@
+package util.fileloaders;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import world.templates.PlayerTemplate;
+
+import java.io.InputStream;
+
+public class JsonLoader {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> T load(String path, Class<T> type){
+
+        try (InputStream stream = JsonLoader.class.getClassLoader().getResourceAsStream(path)){
+            if (stream == null){
+                throw new RuntimeException("Resource not found: " + path);
+            }
+
+            return objectMapper.readValue(stream, type);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        PlayerTemplate player = JsonLoader.load("data/players/centre.json", PlayerTemplate.class);
+        System.out.println(player);
+    }
+}
--- src/world/templates/PlayerTemplate.java
+++ src/world/templates/PlayerTemplate.java
@@ -1,4 +1,13 @@
 package world.templates;
 
 public class PlayerTemplate {
+    public int speed;
+
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "speed=" + speed +
+                '}';
+    }
 }
