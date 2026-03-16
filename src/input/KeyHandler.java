02777bd316f9bae1e922c76d42444dce28437601
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-16 13:19:26 +0000

    Implemented empty interface methods.

--- src/input/KeyHandler.java
+++ src/input/KeyHandler.java
@@ -1,4 +1,22 @@
 package input;
 
-public class KeyHandler {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        return;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        return;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        return;
+    }
 }
