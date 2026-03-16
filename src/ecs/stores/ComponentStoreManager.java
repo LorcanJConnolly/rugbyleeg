817d692e4c09bff7b622d5653171928412e6eea1
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-16 17:05:11 +0000

    Copied implementation of sparse-array component store manager from previous project.

--- src/ecs/stores/ComponentStoreManager.java
+++ src/ecs/stores/ComponentStoreManager.java
@@ -1,4 +1,55 @@
 package ecs.stores;
+import ecs.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Manages the IDs of component stores in the world.
+ */
 public class ComponentStoreManager {
+
+    private final Map<Class<? extends Component>, ComponentStore<? extends Component>> stores = new HashMap<>();
+
+    public <T extends Component> void register(Class<T> componentType, ComponentStore<T> store) {
+        stores.put(componentType, store);
+    }
+
+    public <T extends Component> boolean entityHas(int entity, Class<T> componentType) {
+        return stores.get(componentType).has(entity);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Component> ComponentStore<T> getStore(Class<T> componentType) {
+        return (ComponentStore<T>) stores.get(componentType);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Component> T getComponent(int entity, Class<T> componentType) {
+        ComponentStore<T> store = (ComponentStore<T>) stores.get(componentType);
+        if (store == null) return null;
+        return store.get(entity);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Component> void addComponent(int entity, Class<T> componentType, T component) {
+        ComponentStore<T> store = (ComponentStore<T>) stores.get(componentType);
+        if (store == null) return;
+        store.add(entity, component);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Component> void removeComponent(int entity, Class<T> componentType) {
+        ComponentStore<T> store = (ComponentStore<T>) stores.get(componentType);
+        if (store == null) return;
+        store.remove(entity);
+    }
+
+    public void onEntityDestroyed(int entity){
+        for (ComponentStore<? extends Component> store : stores.values()){
+            store.remove(entity);
+        }
+    }
 }
+
+
