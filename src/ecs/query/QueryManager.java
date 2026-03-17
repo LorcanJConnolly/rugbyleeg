c31814445f9462aa4e640ab80888902d9c0f72ab
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-17 19:04:58 +0000

    Implemented QueryManager following on from previous project including new QueryKey methods.

--- src/ecs/query/QueryManager.java
+++ src/ecs/query/QueryManager.java
@@ -1,4 +1,49 @@
 package ecs.query;
 
+import ecs.Component;
+import ecs.stores.ComponentStore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class QueryManager {
+
+    private final Map<QueryKey, Query> queries = new HashMap<>();
+
+    @SafeVarargs
+    public final <T extends Component> Query getOrCreateQuery(ComponentStore<T>... stores){
+        QueryKey key = new QueryKey();
+        Query query = queries.get(key);
+        if (query != null){
+            return query;
+        } else {
+            Query newQuery = new Query(stores);
+            queries.put(key, newQuery);
+            return newQuery;
+        }
+    }
+
+    public void onComponentAdded(int entity, ComponentStore<? extends Component> store){
+        for (QueryKey query_key : queries.keySet()){
+            if (query_key.getStores().contains(store)){
+                queries.get(query_key).onComponentAdded(entity, store);
+            }
+        }
+    }
+
+    public void onComponentRemoved(int entity, ComponentStore<? extends Component> store){
+        for (QueryKey query_key : queries.keySet()){
+            if (query_key.getStores().contains(store)){
+                queries.get(query_key).onComponentRemoved(entity, store);
+            }
+        }
+    }
+
+    public void onEntityDestroyed(int entity){
+        for (Query query : queries.values()){
+            if (query.getResults().contains(entity)){
+                query.onEntityDestroyed(entity);
+            }
+        }
+    }
 }
