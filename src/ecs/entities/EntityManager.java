a3f98c4380328dbc5240342d8b268176d79ca591
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-17 19:38:45 +0000

    Basic implementation.

--- src/ecs/entities/EntityManager.java
+++ src/ecs/entities/EntityManager.java
@@ -1,4 +1,16 @@
 package ecs.entities;
 
 public class EntityManager {
+    private final int MAX_ENTITES;
+    private int nextId = 0;
+
+    public EntityManager(int maxEntities){
+        this.MAX_ENTITES = maxEntities;
+    }
+
+    public int getId(){
+        return nextId++;
+    }
+
+
 }
