df24c251747f51e660dcf5fbd0583625aff22f8b
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-17 19:38:33 +0000

    Began implementation of game screen attributes.

--- src/core/Game.java
+++ src/core/Game.java
@@ -1,4 +1,46 @@
 package core;
 
-public class Game {
+import ecs.World;
+import input.KeyHandler;
+import world.WorldBuilder;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Renders the game and implements the game loop.
+ * Extends JPanel to implement the game's screen.
+ */
+public class Game extends JPanel implements Runnable{
+    // Screen settings.
+    final int originalTileSize = 16;
+    final int tileScale = 3;
+    public final int tileSize = originalTileSize * tileScale;
+    final int maxScreenCol = 32;
+    final int maxScreenRow = 24;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    int FPS = 60;
+    Thread gameThread;
+
+    KeyHandler keyH = new KeyHandler();
+    World world;
+
+    public Game(){
+        // JPanel game screen configuration.
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);   // Drawing from this ecs.component will be done in an offscreen painting buffer -- improves game's rendering performance.
+        this.addKeyListener(keyH);
+        this.setFocusable(true);        // core.systems.GamePanel can be "focused" to receive key input.
+
+        WorldBuilder worldBuilder = new WorldBuilder();
+//        world = worldBuilder.load("data/");
+    }
+
+    @Override
+    public void run() {
+        return;
+    }
 }
