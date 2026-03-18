f6f8ca73b2165bdca98508c887e535bf868abd85
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-18 07:34:56 +0000

    Implemented buttons from previous project.

--- src/input/GameAction.java
+++ src/input/GameAction.java
@@ -1,4 +1,19 @@
 package input;
 
 public enum GameAction {
+    MOVE_UP,
+    MOVE_DOWN,
+    MOVE_LEFT,
+    MOVE_RIGHT,
+    LEFT_ARM,
+    RIGHT_ARM,
+    ALT_MODIFIER,
+    OPTION_1,
+    OPTION_2,
+    OPTION_3,
+    OPTION_4,
+    OPTION_5,
+    ACCEPT,
+    DECLINE,
+    DEBUG; // A button used for testing to trigger wanted effects without building all supporting systems.
 }
