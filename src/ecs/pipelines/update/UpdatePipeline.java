ac11960f7449bf9b577f56e844f966be7538252e
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-18 07:58:09 +0000

    Added update method.

--- src/ecs/pipelines/update/UpdatePipeline.java
+++ src/ecs/pipelines/update/UpdatePipeline.java
@@ -1,4 +1,8 @@
 package ecs.pipelines.update;
 
 public class UpdatePipeline {
+
+    public void update(double dt){
+        return;
+    }
 }
--- src/ecs/pipelines/update/UpdateSystem.java
+++ src/ecs/pipelines/update/UpdateSystem.java
@@ -1,4 +1,6 @@
 package ecs.pipelines.update;
 
 public interface UpdateSystem {
+
+    public void update(double dt);
 }
