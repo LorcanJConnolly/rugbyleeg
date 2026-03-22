42d2a34099a9279be0286d5ab387f8dbead4ec67
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-22 09:21:56 +0000

    Began to implement cases.

--- src/input/KeyHandler.java
+++ src/input/KeyHandler.java
@@ -1,13 +1,21 @@
 package input;
 
+import components.inputs.Inputs;
+
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
+    private Inputs inputs;
+
+    public void bind(Inputs input) {
+        this.inputs = input;
+    }
+
 
     @Override
     public void keyPressed(KeyEvent e) {
-        return;
+        case KeyEvent.VK_W -> inputs.current.put(GameAction.MOVE_UP, true);
     }
 
     @Override
