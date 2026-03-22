03b49b836d93853f1081156e3d396ce86400783c
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-22 09:20:22 +0000

    Implemented InputSystem from previous project.

---
+++ src/systems/inputs/InputSystem.java
@@ -0,0 +1,39 @@
+package systems.inputs;
+
+import components.inputs.Inputs;
+import ecs.World;
+import ecs.pipelines.update.UpdateSystem;
+import ecs.query.Query;
+import input.GameAction;
+
+public class InputSystem implements UpdateSystem {
+    private final World world;
+    private final Query query;
+
+    public InputSystem(World world){
+        this.world = world;
+        this.query = world.query(Inputs.class);
+    }
+
+    @Override
+    public void update(double dt) {
+        query.forEach((entity, input) -> process((Inputs) input));
+    }
+
+    public void process(Inputs input) {
+        for (GameAction button: GameAction.values()){
+            boolean currentButtonState = input.current.get(button);
+            boolean previousButtonState = input.previous.get(button);
+
+            // Is the button is pressed (went down)
+            input.pressed.put(button, currentButtonState && !previousButtonState);
+            // Is the button is released (went up)
+            input.released.put(button, !currentButtonState && previousButtonState);
+            // Is the button held (is down)
+            input.held.put(button, currentButtonState);
+
+            // Advance current -> previous for next frame.
+            input.previous.put(button, currentButtonState);
+        }
+    }
+}
