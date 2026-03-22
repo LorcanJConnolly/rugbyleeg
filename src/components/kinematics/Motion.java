89dd287c3e798c7a5cf16fb5a87a7bf2a97e5306
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-22 10:25:30 +0000

    Init components.

---
+++ src/components/kinematics/Motion.java
@@ -0,0 +1,8 @@
+package components.kinematics;
+
+import util.vectors.Vector2;
+
+public class Motion {
+    Vector2 velocity;
+    double rotation;
+}
---
+++ src/components/kinematics/Transform.java
@@ -0,0 +1,8 @@
+package components.kinematics;
+
+import util.vectors.Vector2;
+
+public class Transform {
+    Vector2 position;
+    double orientation;
+}
