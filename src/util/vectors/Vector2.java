952fc673c3f17e3f9fefb17edfd7b5bc1cbe0e4f
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-03-23 08:03:14 +0000

    Added basic implementation.

--- src/util/vectors/Vector2.java
+++ src/util/vectors/Vector2.java
@@ -1,4 +1,20 @@
 package util.vectors;
 
 public class Vector2 {
+    public double x;
+    public double y;
+
+    public Vector2() {
+        this(0, 0);
+    }
+
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector2(" + x + ", " + y + ")" ;
+    }
 }
