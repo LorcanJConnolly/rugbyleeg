cf61ba7a805563096e0a28ba3056260662c6e1ad
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-04 17:19:43 +0100

    Added helpers.

--- src/components/team/phase/Phase.java
+++ src/components/team/phase/Phase.java
@@ -1,10 +1,19 @@
 package components.team.phase;
 
+import ecs.Component;
 import rugby.team.Team;
 
 /**
  * A component for describing a team entity's current "game state" or "game phase" (attacking or defending).
  */
-public class Phase {
+public class Phase implements Component {
     public Team phase; // ATTACKING or DEFENDING
+
+    public boolean isAttacking(){
+        return phase == Team.ATTACK;
+    }
+
+    public boolean isDefending(){
+        return phase == Team.DEFENCE;
+    }
 }
