2bcbdaee149221d5ac7f2deb8d24b94587e0fdd6
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-04 11:34:15 +0100

    Created methods for calculating positions on the pitch using relative factors of the pitch's dimensions.

---
+++ src/util/pitch/PitchUtils.java
@@ -0,0 +1,41 @@
+package util.pitch;
+
+import components.pitch.PitchDimensions;
+import util.vectors.Vector2;
+
+/**
+* A class consisting of static utility methods for preforming pitch related calculations..
+*/
+public class PitchUtils {
+
+
+    static double relativeToPitchX(PitchDimensions dimensions, double x_factor){
+        return dimensions.leftTouch + dimensions.aabb.width*x_factor;
+    }
+
+
+    static double relativeToPlayingFieldY(PitchDimensions dimensions, double y_factor){
+        return dimensions.topTryLine + dimensions.aabb.height*y_factor;
+    }
+
+
+    static double relativeToPitchY(PitchDimensions dimensions, double y_factor){
+        return dimensions.aabb.origin.y + dimensions.aabb.height*y_factor;
+    }
+
+
+    static Vector2 relativeToPlayingField(PitchDimensions dimensions, double x_factor, double y_factor){
+        return new Vector2(
+                relativeToPitchX(dimensions, x_factor),
+                relativeToPlayingFieldY(dimensions, y_factor)
+        );
+    }
+
+
+    static Vector2 relativeToPitch(PitchDimensions dimensions, double x_factor, double y_factor){
+        return new Vector2(
+                relativeToPitchX(dimensions, x_factor),
+                relativeToPitchY(dimensions, y_factor)
+        );
+    }
+}
