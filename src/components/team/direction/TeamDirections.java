bf829449842bbd016da2023501da95e2f489db82
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-04 17:22:02 +0100

    Implements component.

--- src/components/team/direction/TeamDirections.java
+++ src/components/team/direction/TeamDirections.java
@@ -1,7 +1,8 @@
 package components.team.direction;
 
+import ecs.Component;
 import util.directions.Direction;
 
-public class AttackingPlane {
+public class TeamDirections implements Component {
     public Direction forward, backwards, inside, outside;
 }
