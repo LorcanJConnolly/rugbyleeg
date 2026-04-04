b5add661a6522d181c1112d306fd0bf26ae1a86e
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-04 17:20:09 +0100

    Added basic fields.

--- src/components/pitch/PitchDimensions.java
+++ src/components/pitch/PitchDimensions.java
@@ -2,7 +2,21 @@ package components.pitch;
 
 import ecs.Component;
 import util.shapes.AABB;
+import util.vectors.Vector2;
 
 public class PitchDimensions implements Component {
-    AABB aabb;
+    // Entire pitch (including in goal areas)
+    public AABB aabb;
+    // In play area
+    public double leftTouch;
+    public double rightTouch;
+    public double topTryLine;
+    public double bottomTryLine;
+    // In goal areas
+    public AABB topInGoal;
+    public AABB bottomInGoal;
+    public double topDeadBall;
+    public double bottomDeadBall;
+    // pitch markings
+    public double centre;
 }
