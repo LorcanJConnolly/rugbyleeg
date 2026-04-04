a7a8c5e3eb5ee01aacd5033b7bbe8c505e402446
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-04 17:17:40 +0100

    Added helper functions.

--- src/util/directions/Direction.java
+++ src/util/directions/Direction.java
@@ -4,5 +4,32 @@ public enum Direction {
     UP,
     DOWN,
     LEFT,
-    RIGHT
+    RIGHT;
+
+    public Direction opposite() {
+        return switch (this) {
+            case UP    -> DOWN;
+            case DOWN  -> UP;
+            case LEFT  -> RIGHT;
+            case RIGHT -> LEFT;
+        };
+    }
+
+    public Direction rotateClockwise() {
+        return switch (this) {
+            case UP    -> RIGHT;
+            case RIGHT -> DOWN;
+            case DOWN  -> LEFT;
+            case LEFT  -> UP;
+        };
+    }
+
+    public Direction rotateCounterClockwise() {
+        return switch (this) {
+            case UP    -> LEFT;
+            case LEFT  -> DOWN;
+            case DOWN  -> RIGHT;
+            case RIGHT -> UP;
+        };
+    }
 }
