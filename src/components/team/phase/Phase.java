337d9a4dc71b2d24ec0f4c03f8b0c5542813ea14
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-04 15:42:41 +0100

    Created a Phase component for team entitys to describe the "game state" or "game phase" the team entity is currently in (attacking or defending).

---
+++ src/components/team/phase/Phase.java
@@ -0,0 +1,10 @@
+package components.team.phase;
+
+import rugby.team.Team;
+
+/**
+ * A component for describing a team entity's current "game state" or "game phase" (attacking or defending).
+ */
+public class Phase {
+    public Team phase; // ATTACKING or DEFENDING
+}
