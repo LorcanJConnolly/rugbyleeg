57ccadb4ab71743468487c615c62e271ea394731
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-05 10:41:33 +0100

    Added pipelines back to game after accidentally deleting because I can't read.

---
+++ src/ecs/pipelines/render/RenderPipeline.java
@@ -0,0 +1,10 @@
+package ecs.pipelines.render;
+
+import java.awt.*;
+
+public class RenderPipeline {
+    public void render(Graphics2D g2, double dt){
+        // for system in pipeline, render()
+        return;
+    };
+}
---
+++ src/ecs/pipelines/update/UpdatePipeline.java
@@ -0,0 +1,9 @@
+package ecs.pipelines.update;
+
+public class UpdatePipeline {
+
+    public void update(double dt){
+        // for system in pipeline, update()
+        return;
+    }
+}
