cdd50471649f28e88717ae28bcea82834e5d1948
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-06 10:41:27 +0100

    Added static methods for math operations on two existing instances to get a new instance, and all instance methods for preforming math operations on an existing instance.

--- src/util/vectors/Vector2.java
+++ src/util/vectors/Vector2.java
@@ -4,13 +4,34 @@ public class Vector2 {
     public double x;
     public double y;
 
-    public Vector2() {
-        this(0, 0);
-    }
-
     public Vector2(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    // Static util methods
+    public static Vector2 addTogether(Vector2 v1, Vector2 v2){
+        return new Vector2(v1.x + v2.x, v1.y + v2.y);
+    }
+
+    public Vector2 subtractFrom(Vector2 v1, Vector2 v2){
+        return new Vector2(v1.x - v2.x, v1.y - v2.y);
+    }
+
+    // Instance methods
+    public void add(Vector2 other){
+        x += other.x;
+        y += other.y;
+    }
+
+    public void subtract(Vector2 other){
+        x -= other.x;
+        y -= other.y;
+    }
+
+    public void scale(double factor){
+        x *= factor;
+        y *= factor;
     }
 
     @Override
