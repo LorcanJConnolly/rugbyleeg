a5b718b330ee4b5d4c1af063510530dfd84266a3
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-06 10:46:50 +0100

    Initial implementation using only kinematics.

---
+++ src/world/templates/entities/PlayerTemplate.java
@@ -0,0 +1,11 @@
+package world.templates.entities;
+
+import world.templates.components.KinematicsTemplate;
+
+/**
+ * A POJO template for configuring a player entity's components.
+ */
+public class PlayerTemplate {
+    public KinematicsTemplate kinematics;
+
+}
