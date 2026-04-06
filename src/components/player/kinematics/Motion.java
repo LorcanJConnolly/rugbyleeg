af6291e10e4873a0ce88a1d66ec010023121f917
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-06 10:15:23 +0100

    Added first builder pattern to component for world builder construction.

--- src/components/player/kinematics/Motion.java
+++ src/components/player/kinematics/Motion.java
@@ -6,4 +6,38 @@ import util.vectors.Vector2;
 public class Motion implements Component {
     Vector2 velocity;
     double rotation;
+
+    private Motion(Builder b){
+        this.velocity = b.velocity;
+        this.rotation = b.rotation;
+    }
+
+    // Entry point.
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    // Builder pattern.
+    public static class Builder{
+        // Default values.
+        private Vector2     velocity = new Vector2(0, 0);
+        private double      rotation = 0.0;
+
+        private Builder(){}
+
+        public Builder velocity(Vector2 velocity){
+            this.velocity.add(velocity);
+            return this;
+        }
+
+        public Builder velocity(double x, double y){
+            this.velocity.add(new Vector2(x, y));
+            return this;
+        }
+
+        public Builder rotation(Vector2 rotation){
+            this.velocity.add(velocity);
+            return this;
+        }
+    }
 }
