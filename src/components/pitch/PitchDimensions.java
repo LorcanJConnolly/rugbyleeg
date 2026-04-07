f331a79430d56f4483f99104820634541ea2215f
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-07 17:32:03 +0100

    Implemented PitchDimensions builder.

--- src/components/pitch/PitchDimensions.java
+++ src/components/pitch/PitchDimensions.java
@@ -1,5 +1,6 @@
 package components.pitch;
 
+import components.player.kinematics.Transform;
 import ecs.Component;
 import util.shapes.AABB;
 import util.vectors.Vector2;
@@ -17,4 +18,65 @@ public class PitchDimensions implements Component {
     public AABB bottomInGoal;
     public double topDeadBall;
     public double bottomDeadBall;
+
+    private PitchDimensions(Builder b){
+        this.aabb = b.aabb;
+        this.leftTouch = b.leftTouch;
+        this.rightTouch = b.rightTouch;
+        this.topTryLine = b.topTryLine;
+        this.bottomTryLine = b.bottomTryLine;
+        this.topInGoal = b.topInGoal;
+        this.bottomInGoal = b.bottomInGoal;
+        this.topDeadBall = b.topDeadBall;
+        this.bottomDeadBall = b.bottomDeadBall;
+    }
+
+    // Entry points.
+    public static Builder builder(){
+        return new Builder();
+    }
+
+
+    // Builder pattern.
+    public static class Builder{
+        // Default values.
+        private AABB aabb               = new AABB(new Vector2(0.0, 0.0), 680.0, 1160.0);
+        private double leftTouch        = 0.0;
+        private double rightTouch       = 680.0;
+        private double topTryLine       = 80.0;
+        private double bottomTryLine    = 1080.0;
+        private AABB topInGoal          = new AABB(new Vector2(0.0, 0.0), 680.0, 80.0);
+        private AABB bottomInGoal       = new AABB(new Vector2(0.0, 1080.0), 680.0, 80.0);;
+        private double topDeadBall      = 80;
+        private double bottomDeadBall   = 1160;
+
+
+        private Builder(){}
+
+        public Builder aabb(AABB aabb){
+            this.aabb = aabb;
+            return this;
+        }
+
+        public Builder leftTouch(double leftTouch) { this.leftTouch = leftTouch; return this; }
+
+        public Builder rightTouch(double rightTouch) { this.rightTouch = rightTouch; return this; }
+
+        public Builder topTryLine(double topTryLine) { this.topTryLine = topTryLine; return this; }
+
+        public Builder bottomTryLine(double bottomTryLine) { this.bottomTryLine = bottomTryLine; return this; }
+
+        public Builder topInGoal(AABB topInGoal) { this.topInGoal = topInGoal; return this; }
+
+        public Builder bottomInGoal(AABB bottomInGoal) { this.bottomInGoal = bottomInGoal; return this; }
+
+        public Builder topDeadBall(double topDeadBall) { this.topDeadBall = topDeadBall; return this; }
+
+        public Builder bottomDeadBall(double bottomDeadBall) { this.bottomDeadBall = bottomDeadBall; return this; }
+
+        // Finalise construction - hand builder to private constructor method.
+        public PitchDimensions build(){
+            return new PitchDimensions(this);
+        }
+    }
 }
