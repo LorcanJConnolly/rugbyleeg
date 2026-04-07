ae0ffec3e02e039b1ca0773ea1791d547bf8edda
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-07 17:32:49 +0100

    Implemented.

---
+++ src/world/templates/components/PitchDimensionsTemplate.java
@@ -0,0 +1,16 @@
+package world.templates.components;
+
+import util.shapes.AABB;
+import world.templates.utils.AABBTemplate;
+
+public class PitchDimensionsTemplate {
+    public AABBTemplate aabb;
+    public double leftTouch;
+    public double rightTouch;
+    public double topTryLine;
+    public double bottomTryLine;
+    public AABBTemplate topInGoal;
+    public AABBTemplate bottomInGoal;
+    public double topDeadBall;
+    public double bottomDeadBall;
+}
