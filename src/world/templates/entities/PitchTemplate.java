42e9b9a2b8760981574cca5a73b5eabe9b5b00a8
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-07 17:32:18 +0100

    Implemented.

--- src/world/templates/entities/PitchTemplate.java
+++ src/world/templates/entities/PitchTemplate.java
@@ -1,7 +1,10 @@
 package world.templates.entities;
 
+import world.templates.components.PitchDimensionsTemplate;
+
 /**
  * A template for configuring the pitch entity singleton's components.
  */
 public class PitchTemplate {
+    PitchDimensionsTemplate dimensions;
 }
