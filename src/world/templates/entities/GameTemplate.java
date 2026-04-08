58af7477bbdeb7c67636f032cf3a64046a711dc9
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-08 08:08:22 +0100

    Added state to template.

--- src/world/templates/entities/GameTemplate.java
+++ src/world/templates/entities/GameTemplate.java
@@ -1,7 +1,10 @@
 package world.templates.entities;
 
+import world.templates.components.GameStateTemplate;
+
 /**
  * A template for configuring the game entity singleton's components.
  */
 public class GameTemplate {
+    GameStateTemplate state;
 }
