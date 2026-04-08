805d245894a50ea042471bf903d063534f3563c8
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-08 08:08:05 +0100

    Deleted unused import.

--- src/world/templates/components/GameStateTemplate.java
+++ src/world/templates/components/GameStateTemplate.java
@@ -3,7 +3,6 @@ package world.templates.components;
 import state.game.GameStates;
 
 import java.util.EnumSet;
-import java.util.List;
 
 public class GameStateTemplate {
     // TODO: Does Jackson read this as expected?
