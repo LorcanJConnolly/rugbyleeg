5e4814c1b5505e4c6636941b5df8e4c2840b86fd
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-09 07:29:11 +0100

    Implemented.

--- src/ecs/eventbus/EventSubscription.java
+++ src/ecs/eventbus/EventSubscription.java
@@ -8,4 +8,32 @@ package ecs.eventbus;
  * necessary without the need for a reference to the eventbus itself.<></>
  */
 public class EventSubscription {
+
+    private final EventBus bus;
+    private final Class<? extends Event> eventType;
+    private final HandlerEntry<?> entry;
+    private boolean active;
+
+
+    public EventSubscription(EventBus bus, Class<? extends Event> eventType, HandlerEntry<?> entry) {
+        this.bus = bus;
+        this.eventType = eventType;
+        this.entry = entry;
+        this.active = true;
+    }
+
+    /**
+     * Removes this subscription's handler entry from the bus' subscriptions.
+     */
+    public void cancel(){
+        if (isActive()){
+            bus.removeHandler(eventType, entry);
+            active = false;
+        }
+    }
+
+
+    public boolean isActive(){
+        return active;
+    }
 }
