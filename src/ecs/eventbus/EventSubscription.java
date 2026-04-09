fa7afa9980481d9244fee18647db218643229206
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-09 07:23:04 +0100

    Added Javadocs describing the class' responsibilities.

--- src/ecs/eventbus/EventSubscription.java
+++ src/ecs/eventbus/EventSubscription.java
@@ -1,4 +1,11 @@
 package ecs.eventbus;
 
+
+/**
+ * A class representation of a live subscription to an event.
+ *
+ * <p></> Used for maintaining a reference to a subscription that allows for the subscription to be cancelled when
+ * necessary without the need for a reference to the eventbus itself.<></>
+ */
 public class EventSubscription {
 }
