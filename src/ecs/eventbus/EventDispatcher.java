74280b73fad76c70f881af76f24fdb926c409fd9
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-09 08:40:40 +0100

    Implemented the interface of the class.

--- src/ecs/eventbus/EventDispatcher.java
+++ src/ecs/eventbus/EventDispatcher.java
@@ -3,6 +3,34 @@ package ecs.eventbus;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+
+/**
+ * Manages the event queue of the eventbus, responsible for maintaining created events and dispatching them to
+ * listeners.
+ */
 public class EventDispatcher {
     private final Queue<Event> queue = new ArrayDeque<>();
+
+
+    /** Enqueue an event for deferred dispatch (i.e., the very end of the frame execution). */
+    public void enqueue(Event event){
+
+    }
+
+
+    public void flush(){
+
+    }
+
+
+
+    public void clear(){
+
+    }
+
+
+
+    public int pendingCount(){
+        return queue.size();
+    }
 }
