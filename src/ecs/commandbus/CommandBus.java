dbeae25fb4d510cd19518aaa20cd626176a0abd2
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-12 10:47:14 +0100

    Implemented issue() method and added a addMiddleware() method for adding middleware to the middelware chain.

--- src/ecs/commandbus/CommandBus.java
+++ src/ecs/commandbus/CommandBus.java
@@ -1,11 +1,33 @@
 package ecs.commandbus;
 
 
+import ecs.eventbus.HandlerEntry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * The central command bus. Responsible for routing commands to their registered handler and running them through their
  * middleware chain before delivery.
  */
 public class CommandBus {
+    // Owns the handlers.
+    // {command type: handler}
+    private final Map<Class<?>, CommandHandler<?>> handlers;
+    // Owns command chains and their middleware
+    private final List<CommandMiddleware<?>> middlewareChain;
+    // Owns the command queue
+    private final CommandQueue queue;
+
+
+    public CommandBus() {
+        this.handlers = new HashMap<>();
+        this.middlewareChain = new ArrayList<>();
+        this.queue = new CommandQueue();
+    }
+
 
     /**
      * Registers a handler responsible for executing commands of a given type. NOTE: only one handler may be
@@ -18,8 +40,19 @@ public class CommandBus {
             Class<T> commandType,
             CommandHandler<T> handler
         ){
-        return;
+        if (handlers.containsKey(commandType)){
+            throw new RuntimeException(
+                    "The command type '" + commandType + "' already has a handler in the command bus."
+            );
+        }
+        handlers.put(commandType, handler);
     }
+
+
+    public <T extends Command> void addMiddleware(CommandMiddleware<T> middleware){
+        middlewareChain.add(middleware);
+    }
+
 
     /**
      * Issues a command for immediate execution by its handler.
@@ -27,8 +60,23 @@ public class CommandBus {
      * @return          A {@link CommandResult} object indicating the result of the command.
      * @param <T>       The command type.
      */
+    @SuppressWarnings("unchecked")
     public <T extends Command> CommandResult issue(T command){
-        return;
+        CommandHandler<T> handler = (CommandHandler<T>) handlers.get(command.getClass());
+        if (handler == null){
+            return CommandResult.failure(
+                    "No handler registered for the command '" + command.getClass().getSimpleName() +"'."
+            );
+        }
+        // Construct the command chain
+        CommandChain<T> chain = handler::handle;
+        for (int i = middlewareChain.size() -1; i >=0; i--) {
+            CommandMiddleware<T> middleware = (CommandMiddleware<T>) middlewareChain.get(i);
+            CommandChain<T> next = chain;
+            // Chain is now a lambda that when called runs middleware.intercept(cmd, next) N times until next = handler.
+            chain = cmd -> middleware.intercept(cmd, next);
+        }
+        return chain.execute(command);
     }
 
 
@@ -38,7 +86,7 @@ public class CommandBus {
      * @param <T>       The command type.
      */
     public <T extends Command> void issueDeferred(T command){
-        return;
+        queue.enqueue(command);
     }
 
 
@@ -48,6 +96,6 @@ public class CommandBus {
      * @param <T>           The command type.
      */
     public <T extends Command> void unregister(Class<T> commandType){
-        return;
+        handlers.remove(commandType);
     }
 }
