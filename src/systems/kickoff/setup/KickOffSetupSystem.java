9091fc931c6448b6f1ba04d2da4241f6b1fce101
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-12 12:24:33 +0100

    Added a method for creating systems and subscribing them to their events and commands.

--- src/systems/kickoff/setup/KickOffSetupSystem.java
+++ src/systems/kickoff/setup/KickOffSetupSystem.java
@@ -1,9 +1,28 @@
 package systems.kickoff.setup;
 
+import ecs.eventbus.EventBus;
+import ecs.eventbus.events.GameReset;
+import ecs.pipelines.update.UpdateSystem;
+
 /**
  * Responsible for configuring the variables of a kick off.
  *
  * <p></> Includes: Which team is attacking and which team is defending.
  */
-public class KickOffSetupSystem {
+public class KickOffSetupSystem implements UpdateSystem {
+
+    @Override
+    public void update(double dt) {
+        return;
+    }
+
+    /** Called during world creation. Subscribers the system to its events. */
+    public void registerSubscription(EventBus eventBus){
+        eventBus.subscribe(
+                GameReset.class,
+                event -> update(event.getDt())
+        );
+    }
+
+
 }
