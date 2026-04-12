0510728f89eba01610da6de6eda5bca7bfe97267
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-12 21:28:33 +0100

    Implemented minimal code to stop errors.

--- src/systems/kickoff/kick/KickOffKickSystem.java
+++ src/systems/kickoff/kick/KickOffKickSystem.java
@@ -1,8 +1,34 @@
 package systems.kickoff.kick;
 
+import components.player.kinematics.Motion;
+import components.player.kinematics.Transform;
+import ecs.World;
+import ecs.pipelines.update.UpdateSystem;
+import ecs.query.Query;
 
 /**
  * Responsible for converting user inputs to kicking the ball at the kick-off.
  */
-public class KickOffKickSystem {
+public class KickOffKickSystem implements UpdateSystem {
+    private final Query query;
+
+    public KickOffKickSystem(World world) {
+        this.query = world.query(Transform.class, Motion.class);
+    }
+
+
+    /**
+     * Receives a KickKickOff command.
+     */
+    @Override
+    public void update(double dt) {
+        query.forEach((int ball, Transform transform, Motion motion) -> {
+            preformKick(transform, motion);
+        });
+    }
+
+
+    public void preformKick(Transform transform, Motion motion){
+        return;
+    }
 }
