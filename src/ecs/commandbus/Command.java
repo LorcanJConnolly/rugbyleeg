4fe00df6ce73c2ee88e2b1a702232ac6874aaf88
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-12 09:18:54 +0100

    Implemented with docstrings.

--- src/ecs/commandbus/Command.java
+++ src/ecs/commandbus/Command.java
@@ -1,4 +1,17 @@
 package ecs.commandbus;
 
-public class Command {
+/**
+ * The base class for all commands. Represents the intent to change something in the game - "do this".
+ *
+ * <p></> Subclasses are immutable objects containing the data the command's handler requires to execute the command.
+ */
+public abstract class Command {
+    private final long timestamp;       // In nanoseconds to match dt.
+    private final int targetEntity;     // Optional
+
+
+    protected Command(long timestamp, int targetEntity) {
+        this.timestamp = timestamp;
+        this.targetEntity = targetEntity;
+    }
 }
--- src/ecs/commandbus/CommandChain.java
+++ src/ecs/commandbus/CommandChain.java
@@ -1,4 +1,22 @@
 package ecs.commandbus;
 
-public class CommandChain {
+/**
+ * Represents the remaining steps in a chain of middleware commands which ends at the command's handler. Passed to each
+ * {@link CommandMiddleware} so it can choose to continue or stop the chain.
+ *
+ * <p></> Structure of the command chian is [Command] -> [N CommandMiddleware] -> [Handler]. <p></>
+ *
+ * @param <T> The type of command issued and flowing through the command chain.
+ */
+@FunctionalInterface
+public interface CommandChain<T extends Command> {
+
+
+    /**
+     * Hands the command to the next step in the command chain.
+     *
+     * @param command   The command to hand forward.
+     * @return          The result of the remainder steps of the chain.
+     */
+    public CommandResult execute(T command);
 }
