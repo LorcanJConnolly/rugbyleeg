a1ffbacaa790187051a15f2d4c626fb32a66f8ad
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-12 15:19:20 +0100

    Renamed field to be consistent with component.

--- src/world/templates/components/GameStateTemplate.java
+++ src/world/templates/components/GameStateTemplate.java
@@ -6,5 +6,5 @@ import java.util.EnumSet;
 
 public class GameStateTemplate {
     // TODO: Does Jackson read this as expected?
-    public EnumSet<GameStates> states;
+    public EnumSet<GameStates> flags;
 }
