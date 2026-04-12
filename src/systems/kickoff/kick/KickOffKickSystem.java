9e8151a13d53be19ad0c1a2de6e86be5c07dab8f
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-12 10:52:03 +0100

    Init files.

---
+++ src/systems/kickoff/kick/KickOffKickSystem.java
@@ -0,0 +1,8 @@
+package systems.kickoff.kick;
+
+
+/**
+ * Responsible for converting user inputs to kicking the ball at the kick-off.
+ */
+public class KickOffKickSystem {
+}
---
+++ src/systems/kickoff/setup/KickOffSetupSystem.java
@@ -0,0 +1,9 @@
+package systems.kickoff.setup;
+
+/**
+ * Responsible for configuring the variables of a kick off.
+ *
+ * <p></> Includes: Which team is attacking and which team is defending.
+ */
+public class KickOffSetupSystem {
+}
