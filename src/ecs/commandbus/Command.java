f5a347dfbeb3c4b2c2590553207a5707340df256
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-12 12:24:58 +0100

    Added dt to constructor.

--- src/ecs/commandbus/Command.java
+++ src/ecs/commandbus/Command.java
@@ -6,12 +6,13 @@ package ecs.commandbus;
  * <p></> Subclasses are immutable objects containing the data the command's handler requires to execute the command.
  */
 public abstract class Command {
-    private  final double dt;                  // Record dt for system update method inputs.
+    private final double dt;                  // Record dt for system update method inputs.
     private final long timestamp;       // In nanoseconds to match dt.
     private final int targetEntity;     // Optional
 
 
     protected Command(double dt, long timestamp, int targetEntity) {
+        this.dt = dt;
         this.timestamp = timestamp;
         this.targetEntity = targetEntity;
     }
