2a5780f3a1a09b39ec2d4257b00522269861c79b
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-13 07:34:53 +0100

    Implementation of a simple KickKickOff system that allows the user to select the x and y coordinate of where the ball lands and also the height of the ball.

--- src/ecs/commandbus/commands/KickKickOff.java
+++ src/ecs/commandbus/commands/KickKickOff.java
@@ -5,9 +5,11 @@ import util.vectors.Vector2;
 
 public class KickKickOff extends Command {
     public Vector2 target;
+    public double angle;
 
-    public KickKickOff(double dt, long timestamp, Vector2 target) {
+    public KickKickOff(double dt, long timestamp, Vector2 target, double angle) {
         super(dt, timestamp);
         this.target = target;
+        this.angle = angle;
     }
 }
--- src/systems/kickoff/kick/KickOffInput.java
+++ src/systems/kickoff/kick/KickOffInput.java
@@ -13,6 +13,7 @@ import ecs.commandbus.commands.KickKickOff;
 import ecs.pipelines.update.UpdateSystem;
 import input.Button;
 import state.game.GameStates;
+import util.directions.Direction;
 import util.pitch.PitchUtils;
 import util.vectors.Vector2;
 
@@ -25,64 +26,78 @@ public class KickOffInput implements UpdateSystem {
     private final Inputs inputs;
     private final Member team;
 
-    private final PitchDimensions pitchDimensions;
-
-    private final Directions attackDirections;
     private final GameState gameState;
     private final CommandBus commandBus;
+    private final PitchDimensions pitchDimensions;
+    private final Directions attackDirections;
 
-    private double x  = -1;
-    private double y   = -1;
+    double x, y, angle;
+    double max_x, max_y, max_angle;
+    double min_x, min_y, min_angle;
+    double increment_x, increment_y, increment_angle;
 
     public KickOffInput(World world, CommandBus commandBus){
         this.pitchDimensions = world.getSingleton(PitchDimensions.class);
-
         SingletonEntities singletonEntities = world.getEntityComponent(
                 world.getSingletonEntity(GameState.class), SingletonEntities.class
         );
         this.attackDirections = world.getEntityComponent(singletonEntities.getAttack(), Directions.class);
-
-
         this.gameState = world.getSingleton(GameState.class);
         int player = world.getSingletonEntity(Inputs.class);
         this.inputs = world.getEntityComponent(player, Inputs.class);
         this.team = world.getEntityComponent(player, Member.class);
         this.commandBus = commandBus;
+
+        this.angle = 45.0;
+        this.min_angle = 10.0;
+        this.max_angle = 80.0;
+        this.increment_angle = 5.0;
+
+        this.x = pitchDimensions.aabb.width/2;
+        this.min_x = attackDirections.forward == Direction.UP ? 1.0 : pitchDimensions.aabb.width;
+        this.max_x = attackDirections.forward == Direction.UP ? pitchDimensions.aabb.width : 1.0;
+        this.increment_x = attackDirections.forward == Direction.UP ? 10.0 : -10.0;
+
+        this.y = attackDirections.forward == Direction.UP ? pitchDimensions.aabb.height/4 : 3*pitchDimensions.aabb.height/4;
+        this.min_y = attackDirections.forward == Direction.UP ? pitchDimensions.aabb.height/2 : 1.0;
+        this.max_y = attackDirections.forward == Direction.UP ? 1.0 : pitchDimensions.aabb.height;
+        this.increment_y = attackDirections.forward == Direction.UP ? 10.0 : -10.0;
     }
 
     @Override
     public void update(double dt) {
         if (!gameState.hasFlag(GameStates.KICK_OFF)) return;
 
-        Map<Button, Boolean>  current = inputs.current;
-        if (x == -1){
-            if (current.get(Button.OPTION_1)){
-                x = 0.05;
-            } else if (current.get(Button.OPTION_2)) {
-                x = 0.15;
-            } else if (current.get(Button.OPTION_3)) {
-                x = 0.25;
-            } else if (current.get(Button.OPTION_4)) {
-                x = 0.35;
-            } else if (current.get(Button.OPTION_5)) {
-                x = 0.45;
+        if (inputs.pressed.get(Button.OPTION_1) || inputs.held.get(Button.OPTION_1)
+        ) {
+            if (inputs.pressed.get(Button.MOVE_UP) || inputs.held.get(Button.MOVE_UP)) {
+                x += increment_x * dt;
+
+            } else if (inputs.pressed.get(Button.MOVE_DOWN) || inputs.held.get(Button.MOVE_DOWN)) {
+                x += -increment_x * dt;
             }
-        } else if (y == -1) {
-            if (current.get(Button.OPTION_1)){
-                y = 0.05;
-            } else if (current.get(Button.OPTION_2)) {
-                y = 0.15;
-            } else if (current.get(Button.OPTION_3)) {
-                y = 0.25;
-            } else if (current.get(Button.OPTION_4)) {
-                y = 0.35;
-            } else if (current.get(Button.OPTION_5)) {
-                y = 0.45;
+            x = Math.max(Math.min(x, max_x), min_x);
+        } else if (inputs.pressed.get(Button.OPTION_2) || inputs.held.get(Button.OPTION_2)) {
+            if (inputs.pressed.get(Button.MOVE_UP) || inputs.held.get(Button.MOVE_UP)) {
+                y += increment_y * dt;
+
+            } else if (inputs.pressed.get(Button.MOVE_DOWN) || inputs.held.get(Button.MOVE_DOWN)) {
+                y += -increment_y * dt;
             }
-        } else {
-            Vector2 target = PitchUtils.relativeToPlayingField(pitchDimensions, attackDirections.forward, x, 0.4 + y);
-            // create kick
-            commandBus.issue(new KickKickOff(dt, System.nanoTime(), target));
+            y = Math.max(Math.min(y, max_y), min_y);
+        } else if (inputs.pressed.get(Button.OPTION_3) || inputs.held.get(Button.OPTION_3)) {
+            if (inputs.pressed.get(Button.MOVE_UP) || inputs.held.get(Button.MOVE_UP)){
+                angle += increment_angle * dt;
+
+            } else if (inputs.pressed.get(Button.MOVE_DOWN) || inputs.held.get(Button.MOVE_DOWN)) {
+                angle += -increment_angle * dt;
+            }
+            angle = Math.max(Math.min(angle, max_angle), min_angle);
+        }
+
+        if (inputs.pressed.get(Button.ACCEPT) || inputs.held.get(Button.ACCEPT)) {
+            Vector2 target = new Vector2(x, y);
+            commandBus.issue(new KickKickOff(dt, System.nanoTime(), target, angle));
         }
     }
 }
