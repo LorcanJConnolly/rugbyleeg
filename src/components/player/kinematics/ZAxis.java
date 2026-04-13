0f3af92980cf2d8869925320e09581aa77b8a1ad
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-13 13:19:57 +0100

    Renamed component fields.

--- src/components/player/kinematics/ZAxis.java
+++ src/components/player/kinematics/ZAxis.java
@@ -4,5 +4,5 @@ package components.player.kinematics;
  * A component for a 2.5D game for faking altitude and related values.
  */
 public class ZAxis {
-    public double v_z, pos_z;
+    public double velocity, position;
 }
