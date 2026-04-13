d9d3ae07a7b581db1791fdfe80ce553c702b5d21
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-13 07:54:37 +0100

    Updated interface -> now includes methods for accepting commands from the commandbus and events from the eventbus.

--- src/ecs/pipelines/update/UpdateSystem.java
+++ src/ecs/pipelines/update/UpdateSystem.java
@@ -1,6 +1,13 @@
 package ecs.pipelines.update;
 
+import ecs.commandbus.Command;
+import ecs.eventbus.EventBus;
+
 public interface UpdateSystem {
 
     public void update(double dt);
+
+    public void registerListeners(Command bus);
+
+    public void registerSubscriptions(EventBus bus);
 }
