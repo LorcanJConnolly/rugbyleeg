899faeed54ec032043aff5fc16981ea719b68d3b
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-13 13:21:09 +0100

    Implements component.

--- src/components/player/kinematics/ZAxis.java
+++ src/components/player/kinematics/ZAxis.java
@@ -1,8 +1,10 @@
 package components.player.kinematics;
 
+import ecs.Component;
+
 /**
  * A component for a 2.5D game for faking altitude and related values.
  */
-public class ZAxis {
+public class ZAxis implements Component {
     public double velocity, position;
 }
