28779f546c353a5fbbfee623fbbdfcc31127a503
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-13 07:55:42 +0100

    Fixed typing.

--- src/ecs/pipelines/update/UpdateSystem.java
+++ src/ecs/pipelines/update/UpdateSystem.java
@@ -1,13 +1,13 @@
 package ecs.pipelines.update;
 
-import ecs.commandbus.Command;
+import ecs.commandbus.CommandBus;
 import ecs.eventbus.EventBus;
 
 public interface UpdateSystem {
 
     public void update(double dt);
 
-    public void registerListeners(Command bus);
+    public void registerListeners(CommandBus bus);
 
     public void registerSubscriptions(EventBus bus);
 }
