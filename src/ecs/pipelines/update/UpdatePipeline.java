84fac771129689296c227a7154ead549c12de1d4
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-13 16:15:18 +0100

    Basic impl.

--- src/ecs/pipelines/update/UpdatePipeline.java
+++ src/ecs/pipelines/update/UpdatePipeline.java
@@ -1,13 +1,19 @@
 package ecs.pipelines.update;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UpdatePipeline {
+    public final List<UpdateSystem> systems = new ArrayList<>();
+
 
     public void update(double dt){
-        // for system in pipeline, update()
-        return;
+        for (UpdateSystem system : systems) {
+            system.update(dt);
+        }
     }
 
     public <T extends UpdateSystem> void add(T system) {
-        return;
+        systems.add(system);
     }
 }
