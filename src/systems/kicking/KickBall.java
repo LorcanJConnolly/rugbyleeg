338475d00185c8d3975cbefe8c551f0a929929cb
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-15 13:45:59 +0100

    kick the kick-off system is now handled by a system for kicking the ball.

--- src/systems/kicking/KickBall.java
+++ src/systems/kicking/KickBall.java
@@ -1,20 +1,19 @@
-package systems.kickoff.kick;
+package systems.kicking;
 
+import components.game.SingletonEntities;
 import components.kinematics.Motion;
 import components.kinematics.ZAxis;
-import components.game.SingletonEntities;
 import ecs.World;
 import ecs.commandbus.CommandBus;
 import ecs.commandbus.CommandResult;
-import ecs.commandbus.commands.KickBall;
 import ecs.eventbus.EventBus;
 import ecs.pipelines.update.UpdateSystem;
 import util.vectors.Vector2;
 
 /**
- * Responsible for converting a "kick the kick off" command to a kick on the ball.
+ * A system for setting the ball's velocity data after it has been kicked.
  */
-public class KickOffKickSystem implements UpdateSystem {
+public class KickBall implements UpdateSystem {
     private double velocity;
     private double theta_x;
     private double theta_z;
@@ -22,7 +21,7 @@ public class KickOffKickSystem implements UpdateSystem {
     private final ZAxis ball_z;
     private final Motion ball_motion;
 
-    public KickOffKickSystem(World world) {
+    public KickBall(World world) {
         int ball = world.getSingleton(SingletonEntities.class).getBall();
         this.ball_z = world.getEntityComponent(ball, ZAxis.class);
         this.ball_motion = world.getEntityComponent(ball, Motion.class);
@@ -38,11 +37,11 @@ public class KickOffKickSystem implements UpdateSystem {
     @Override
     public void registerListeners(CommandBus bus){
         bus.register(
-            KickBall.class,
-            command -> {
-                preformKick(command);
-                return CommandResult.success();
-            }
+                ecs.commandbus.commands.KickBall.class,
+                command -> {
+                    preformKick(command);
+                    return CommandResult.success();
+                }
         );
     }
 
@@ -52,8 +51,11 @@ public class KickOffKickSystem implements UpdateSystem {
         // System accepts commands.
     }
 
-
-    public void preformKick(KickBall command){
+    /**
+     * Sets ball's x, y, and z velocity components based off a kick command.
+     * @param command
+     */
+    public void preformKick(ecs.commandbus.commands.KickBall command){
         this.velocity = command.velocity;
         this.theta_x = command.theta_x;
         this.theta_z = command.theta_z;
@@ -66,6 +68,6 @@ public class KickOffKickSystem implements UpdateSystem {
                 v_xy*Math.cos(Math.toRadians(theta_x)),
                 v_xy*Math.sin(Math.toRadians(theta_x))
         );
-        ball_z.velocity = v_z; 
+        ball_z.velocity = v_z;
     }
 }
