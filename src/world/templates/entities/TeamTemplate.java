e627d48361fe019aae8fb794e857bb94ac6213bb
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-16 13:32:32 +0100

    Fixed typo.

--- src/world/templates/entities/TeamTemplate.java
+++ src/world/templates/entities/TeamTemplate.java
@@ -6,6 +6,6 @@ import world.templates.components.DirectionTemplate;
  * A template for configuring one of the two team entity's components.
  */
 public class TeamTemplate {
-    public DirectionTemplate direction;
+    public DirectionTemplate directions;
 
 }
