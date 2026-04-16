05b887d497d2a5bd4c5423f9503d444b61db68eb
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-16 22:32:27 +0100

    Basic implementation.

--- src/ecs/pipelines/render/RenderPipeline.java
+++ src/ecs/pipelines/render/RenderPipeline.java
@@ -1,14 +1,21 @@
 package ecs.pipelines.render;
 
+import ecs.pipelines.update.UpdateSystem;
+
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RenderPipeline {
+    public final List<RenderSystem> systems = new ArrayList<>();
+
     public void render(Graphics2D g2, double dt){
-        // for system in pipeline, render()
-        return;
+        for (RenderSystem system : systems) {
+            system.render(g2, dt);
+        }
     };
 
     public <T extends RenderSystem> void add(T system) {
-        return;
+        systems.add(system);
     }
 }
