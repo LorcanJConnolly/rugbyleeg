5f10257f1f2e9d19e3f5794bdcc3fa23f254e531
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-16 19:44:20 +0100

    Made public and added constructor.

--- src/ecs/commandbus/commands/SetUpKickOff.java
+++ src/ecs/commandbus/commands/SetUpKickOff.java
@@ -5,7 +5,11 @@ import ecs.commandbus.Command;
 public class SetUpKickOff extends Command {
 
 
-    protected SetUpKickOff(double dt, long timestamp, int targetEntity) {
+    public SetUpKickOff(double dt, long timestamp, int targetEntity) {
         super(dt, timestamp, targetEntity);
+    }
+
+    public SetUpKickOff(double dt, long timestamp) {
+        super(dt, timestamp);
     }
 }
