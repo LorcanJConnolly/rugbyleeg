fd41ba1dba9d8c9f492040635669aaef73adaaec
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-19 15:11:52 +0100

    Added a look-up for getting a player in a given position on a team.

---
+++ src/components/rugby/position/PositionRegistry.java
@@ -0,0 +1,23 @@
+package components.rugby.position;
+
+import ecs.Component;
+import rugby.positions.Position;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** A team entity component for looking up which entity is in a position */
+public class PositionRegistry implements Component {
+    private final Map<Position, Integer> data = new HashMap<>();
+
+    public Integer getEntity(Position position){
+        return data.get(position);
+    }
+
+    public void register(Position position, int entity){
+        if (getEntity(position) != null) {
+            throw new RuntimeException("The position '" + position + "' is already in the registry.");
+        }
+        data.put(position, entity);
+    }
+}
