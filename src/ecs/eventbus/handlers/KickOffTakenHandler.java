416514f0894329339268818736b2d562566adafd
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-19 11:39:22 +0100

    Fixed constructor.

--- src/ecs/eventbus/handlers/KickOffTakenHandler.java
+++ src/ecs/eventbus/handlers/KickOffTakenHandler.java
@@ -1,14 +1,13 @@
 package ecs.eventbus.handlers;
 
 import ecs.eventbus.EventHandler;
-import ecs.eventbus.events.KickOffLinedUp;
 import ecs.eventbus.events.KickOffTaken;
 import systems.update.game.GameStateSystem;
 
 public class KickOffTakenHandler implements EventHandler<KickOffTaken> {
     private final GameStateSystem gameStateSystem;
 
-    public KickOffLinedUpHandler(GameStateSystem gameStateSystem) {
+    public KickOffTakenHandler(GameStateSystem gameStateSystem) {
         this.gameStateSystem = gameStateSystem;
     }
 
