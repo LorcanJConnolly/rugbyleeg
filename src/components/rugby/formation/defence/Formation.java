d0b850b6c8dc9bd88280838de34dc981e9a1885b
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-28 06:58:13 +0100

    Docstrings and impl component class.

--- src/components/rugby/formation/defence/Formation.java
+++ src/components/rugby/formation/defence/Formation.java
@@ -1,10 +1,12 @@
 package components.rugby.formation.defence;
 
+import ecs.Component;
 import rugby.formation.defence.Slot;
 
 import java.util.List;
 
-public class Formation {
+/** A component representing a team entity's formation data. */
+public class Formation implements Component {
     public double leftBoundary, rightBoundary;
     public List<Slot> slots;
 }
