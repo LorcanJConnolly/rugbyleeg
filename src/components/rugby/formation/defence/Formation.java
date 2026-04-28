193153c2fd65a0e1ac5f489e841442c78e58f6c3
LorcanJConnolly <lconnolly.lorcan@gmail.com>
2026-04-28 06:57:14 +0100

    Initial implementation of formation component and slot class.

---
+++ src/components/rugby/formation/defence/Formation.java
@@ -0,0 +1,10 @@
+package components.rugby.formation.defence;
+
+import rugby.formation.defence.Slot;
+
+import java.util.List;
+
+public class Formation {
+    public double leftBoundary, rightBoundary;
+    public List<Slot> slots;
+}
---
+++ src/rugby/formation/defence/Slot.java
@@ -0,0 +1,8 @@
+package rugby.formation.defence;
+
+/** A class representation of a slot in a formation. */
+public class Slot {
+    public int slot;
+    public double x;
+    public int host;
+}
